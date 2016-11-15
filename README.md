efaf684b5a8e92497cd4ca40079b5740268064f3
sdcaulley <stephanie.caulley@gmail.com>
2016-11-15 11:30:46 -0800

    set up html, and function

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Create a 5x5 square of #.
 
 ### A Line (Reduction of A Square)
 Create a line of 5 #.
+
+## A Sideways Triangle
+Create a triangle that starts with one #, grows to four # and then goes back to one #.
--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 var halfOfASquare = function() {
   var message = '#';
+  var pagePlace = document.getElementById('half_square');
+  var div = document.createElement('div');
+  var p;
   for (var row = 1; row <= 5; row++) {
+    var element = '';
     for (var line = 1; line <= 6 - row; line++) {
-      document.write(message);
+      element += message;
     }
-    document.write('<br>');
+    console.log('element ', element);
+    p = document.createElement('p');
+    p.textContent = element;
+    div.appendChild(p);
   }
+  pagePlace.appendChild(div);
+};
+
+var sidewaysTriangle = function() {
+  var message = '#';
+  var pagePlace = document.getElementById('sideways_triangle');
+  var div = document.createElement('div');
+  var p;
+  
 };
 
 halfOfASquare();
