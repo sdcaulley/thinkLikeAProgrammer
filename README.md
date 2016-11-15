c8e2c1eeb0cb5e0629dc03555de2cb40dfed405f
sdcaulley <stephanie.caulley@gmail.com>
2016-11-15 10:51:01 -0800

    added exercise description to README, loop to print the line

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Create half of a 5x5 square of #.
 
 ### A Square (Reduction of Half of a Square)
 Create a 5x5 square of #.
+
+### A Line (Reduction of A Square)
+Create a line of 5 #.
--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var halfOfASquare = function() {
   var message = '#';
-  for (var row = 0; row < 5; row++) {
-    for (var line = 0; line < 5; line++) {
-      document.write(message);
-    }
-    document.write('<br>');
+  for (var line = 0; line < 5; line++) {
+    document.write(message);
   }
+  document.write('<br>');
 };
 
 halfOfASquare();
